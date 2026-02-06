1bcefa620db07e3fb2d8ed46c43ac1946142279b
WadaNanase <nanase-wada@with21.jp>
2026-02-06 14:17:31 +0900

    feat:add system.in

--- test.java
+++ test.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class test{
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in,"MS932");
         System.out.println("VScode テスト");
+        System.out.println("入力してください");
+        String sentence = scanner.nextLine();
+        System.out.println(sentence);
+
+        scanner.close();
     }
 }
